ea1889cf1e64e9b673d943f978e7bbdb36bdcea3
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-08 12:58:06 +0100

    Adds Hello World app

---
+++ src/main/java/nl/jqno/paralleljava/Main.java
@@ -0,0 +1,9 @@
+package nl.jqno.paralleljava;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String... args) {
+        get("/hello", (req, res) -> "Hello World!");
+    }
+}
