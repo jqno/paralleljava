dbdf7b7e843813ce53779b7dfbb7921c1141b5ad
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-09 09:57:26 +0100

    Adds logging

--- src/main/java/nl/jqno/paralleljava/Main.java
+++ src/main/java/nl/jqno/paralleljava/Main.java
@@ -1,10 +1,18 @@
 package nl.jqno.paralleljava;
 
-import static spark.Spark.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) {
-        port(getHerokuAssignedPort());
+        var port = getHerokuAssignedPort();
+        log.info("Started on port " + port);
+        port(port);
         get("/hello", (req, res) -> "Hello World!");
     }
 
