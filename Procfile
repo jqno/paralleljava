6879ae40d218084f4c728240de82926115f36675
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-09 09:26:34 +0100

    Enables Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar ./target/parallel-java-1.0-jar-with-dependencies.jar
--- src/main/java/nl/jqno/paralleljava/Main.java
+++ src/main/java/nl/jqno/paralleljava/Main.java
@@ -4,6 +4,15 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String... args) {
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello World!");
+    }
+
+    public static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
