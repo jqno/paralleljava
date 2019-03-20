5e9928f351cf04d6689e4502aa881129b652cdb0
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-20 17:39:41 +0100

    Fixes NoClassDefError in gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module nl.jqno.paralleljava {
 
     requires io.vavr;
     requires io.vavr.gson;
+    requires java.sql; // required for gson
     requires gson;
     requires slf4j.api;
     requires spark.core;
