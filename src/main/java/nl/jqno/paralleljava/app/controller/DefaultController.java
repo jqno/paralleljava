2cb7356d040ae3196a4fb2af94cfbee39f243ed1
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-25 08:01:24 +0100

    Flips ifs around for readability

--- src/main/java/nl/jqno/paralleljava/app/controller/DefaultController.java
+++ src/main/java/nl/jqno/paralleljava/app/controller/DefaultController.java
@@ -32,51 +32,55 @@ public class DefaultController implements Controller {
 
     public Try<String> get(String id) {
         var uuid = serializer.deserializeUuid(id);
-        if (uuid.isDefined()) {
-            return repository
-                    .get(uuid.get())
-                    .flatMap(o -> o.map(serializer::serializeTodo).toTry(() -> new IllegalArgumentException("Cannot find " + id)));
+        if (uuid.isEmpty()) {
+            return Try.failure(new IllegalArgumentException("Invalid GET request: " + id));
         }
-        return Try.failure(new IllegalArgumentException("Invalid GET request: " + id));
+
+        return repository
+                .get(uuid.get())
+                .flatMap(o -> o.map(serializer::serializeTodo).toTry(() -> new IllegalArgumentException("Cannot find " + id)));
     }
 
     public Try<String> post(String json) {
         logger.forProduction("POSTed: " + json);
         var partialTodo = serializer.deserializePartialTodo(json);
-        if (partialTodo.isDefined() && partialTodo.get().title().isDefined()) {
-            var pt = partialTodo.get();
-            var id = generator.generateId();
-            var todo = new Todo(id, pt.title().get(), buildUrlFor(id), false, pt.order().getOrElse(0));
-            logger.forProduction("Returning from POST: " + json);
-            return repository.createTodo(todo)
-                    .map(ignored -> serializer.serializeTodo(todo));
+        if (partialTodo.isEmpty() || partialTodo.get().title().isEmpty()) {
+            return Try.failure(new IllegalArgumentException("Invalid POST request: " + json));
         }
-        return Try.failure(new IllegalArgumentException("Invalid POST request: " + json));
+
+        var pt = partialTodo.get();
+        var id = generator.generateId();
+        var todo = new Todo(id, pt.title().get(), buildUrlFor(id), false, pt.order().getOrElse(0));
+        return repository.createTodo(todo)
+                .map(ignored -> serializer.serializeTodo(todo));
     }
 
     public Try<String> patch(String id, String json) {
         logger.forProduction("PATCHed: " + json);
         var uuid = serializer.deserializeUuid(id);
         var partialTodo = serializer.deserializePartialTodo(json);
-        if (uuid.isDefined() && partialTodo.isDefined()) {
-            var pt = partialTodo.get();
-            var existingTodo = repository.get(uuid.get());
-            if (existingTodo.isFailure()) {
-                return existingTodo.map(ignored -> "");
-            }
-            if (existingTodo.get().isDefined()) {
-                var todo = existingTodo.get().get();
-                var updatedTodo = new Todo(
-                        todo.id(),
-                        pt.title().getOrElse(todo.title()),
-                        todo.url(),
-                        pt.completed().getOrElse(todo.completed()),
-                        pt.order().getOrElse(todo.order()));
-                repository.updateTodo(updatedTodo);
-                return Try.of(() -> serializer.serializeTodo(updatedTodo));
-            }
+        if (uuid.isEmpty() || partialTodo.isEmpty()) {
+            return Try.failure(new IllegalArgumentException("Invalid PATCH request: " + id + ", " + json));
         }
-        return Try.failure(new IllegalArgumentException("Invalid PATCH request: " + id + ", " + json));
+
+        var pt = partialTodo.get();
+        var existingTodo = repository.get(uuid.get());
+        if (existingTodo.isFailure()) {
+            return existingTodo.map(ignored -> "");
+        }
+        if (existingTodo.get().isEmpty()) {
+            return Try.failure(new IllegalArgumentException("Can't find Todo with id " + id));
+        }
+
+        var todo = existingTodo.get().get();
+        var updatedTodo = new Todo(
+                todo.id(),
+                pt.title().getOrElse(todo.title()),
+                todo.url(),
+                pt.completed().getOrElse(todo.completed()),
+                pt.order().getOrElse(todo.order()));
+        repository.updateTodo(updatedTodo);
+        return Try.of(() -> serializer.serializeTodo(updatedTodo));
     }
 
     public Try<String> delete() {
@@ -86,11 +90,12 @@ public class DefaultController implements Controller {
 
     public Try<String> delete(String id) {
         var uuid = serializer.deserializeUuid(id);
-        if (uuid.isDefined()) {
-            return repository.delete(uuid.get())
-                    .map(ignored -> "");
+        if (uuid.isEmpty()) {
+            return Try.failure(new IllegalArgumentException("Invalid DELETE request: " + id));
         }
-        return Try.failure(new IllegalArgumentException("Invalid DELETE request: " + id));
+
+        return repository.delete(uuid.get())
+                .map(ignored -> "");
     }
 
     private String buildUrlFor(UUID id) {
