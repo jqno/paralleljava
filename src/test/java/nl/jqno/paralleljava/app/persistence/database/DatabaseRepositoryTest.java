94d407672bacec3ae5a859bf6a60b1427867c3ee
Jan Ouwens <jan.ouwens@gmail.com>
2019-03-29 07:15:09 +0100

    Adds test for failure case

--- src/test/java/nl/jqno/paralleljava/app/persistence/database/DatabaseRepositoryTest.java
+++ src/test/java/nl/jqno/paralleljava/app/persistence/database/DatabaseRepositoryTest.java
@@ -1,37 +1,42 @@
 package nl.jqno.paralleljava.app.persistence.database;
 
+import nl.jqno.paralleljava.app.domain.Todo;
 import nl.jqno.paralleljava.dependencyinjection.TestData;
 import nl.jqno.paralleljava.dependencyinjection.TestData.AnotherTodo;
 import nl.jqno.paralleljava.dependencyinjection.TestData.SomeTodo;
 import nl.jqno.paralleljava.dependencyinjection.TestWiring;
 import nl.jqno.paralleljava.dependencyinjection.Wiring;
 import nl.jqno.picotest.Test;
+import org.jdbi.v3.core.statement.StatementContext;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.UUID;
 
 import static org.assertj.vavr.api.VavrAssertions.assertThat;
 
 public class DatabaseRepositoryTest extends Test {
 
+    private static final String IN_MEMORY_DATABASE = DatabaseRepository.DEFAULT_JDBC_URL;
     private final TodoMapper todoMapper = new TodoMapper(TestData.URL_PREFIX);
 
     public void initialization() {
 
         test("a table is created", () -> {
-            var repo = Wiring.databaseRepository(DatabaseRepository.DEFAULT_JDBC_URL, todoMapper, TestWiring.nopLoggerFactory());
+            var repo = Wiring.databaseRepository(IN_MEMORY_DATABASE, todoMapper, TestWiring.nopLoggerFactory());
             var result = repo.initialize();
             assertThat(result).isSuccess();
         });
 
         test("initializing twice is a no-op the second time", () -> {
-            var repo = Wiring.databaseRepository(DatabaseRepository.DEFAULT_JDBC_URL, todoMapper, TestWiring.nopLoggerFactory());
+            var repo = Wiring.databaseRepository(IN_MEMORY_DATABASE, todoMapper, TestWiring.nopLoggerFactory());
             assertThat(repo.initialize()).isSuccess();
             assertThat(repo.initialize()).isSuccess();
         });
     }
 
     public void repository() {
-        var repo = Wiring.databaseRepository(DatabaseRepository.DEFAULT_JDBC_URL, todoMapper, TestWiring.nopLoggerFactory());
+        var repo = Wiring.databaseRepository(IN_MEMORY_DATABASE, todoMapper, TestWiring.nopLoggerFactory());
 
         beforeAll(() -> {
             assertThat(repo.initialize()).isSuccess();
@@ -82,6 +87,26 @@ public class DatabaseRepositoryTest extends Test {
 
             assertThat(result).isSuccess();
             assertThat(repo.getAllTodos()).hasValueSatisfying(l -> assertThat(l).isEmpty());
+        });
+    }
+
+    public void failures() {
+        var failingMapper = new TodoMapper("") {
+            public Todo map(ResultSet rs, StatementContext ctx) throws SQLException {
+                throw new SQLException("Intentional failure");
+            }
+        };
+        var repo = Wiring.databaseRepository(IN_MEMORY_DATABASE, failingMapper, TestWiring.nopLoggerFactory());
+
+        beforeAll(() -> {
+            repo.initialize();
+        });
+
+        test("Query failures cause failed results", () -> {
+            repo.createTodo(SomeTodo.TODO);
+
+            var result = repo.getAllTodos();
+            assertThat(result).isFailure();
         });
     }
 }
